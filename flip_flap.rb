436bd957894a8f08af20815e950fc4b4f49e7282
chenlizhan <chung1350@hotmail.com>
2014-10-05 20:48:20 +0800

    Complete Homework

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,7 +1,15 @@
 require_relative 'tsv_buddy'
+require 'yaml'
 
+# Class that contains several methods to parse format of data between each other
 class FlipFlap
-
+  include TsvBuddy
   # Do not create any initializers
+  def take_yaml(yml)
+    @data = YAML.load(yml)
+  end
 
+  def to_yaml
+    @data.to_yaml
+  end
 end
